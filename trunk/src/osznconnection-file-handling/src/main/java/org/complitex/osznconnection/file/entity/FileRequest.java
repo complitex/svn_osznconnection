296726a0d7e60bc0c6c1492ccef4a6178392e3ff
artkof.work <artkof.work@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2010-08-24 09:29:54 +0000

    entity added

---
+++ trunk/src/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/entity/FileRequest.java
@@ -0,0 +1,152 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.complitex.osznconnection.file.entity;
+
+/**
+ *
+ * @author Artem
+ */
+public class FileRequest {
+
+    private Long id;
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    private Long fileId;
+
+    private String city;
+
+    private String street;
+
+    private String building;
+
+    private String apartment;
+
+    private Long cityId;
+
+    private Long streetId;
+
+    private Long buildingId;
+
+    private Long apartmentId;
+
+    private Status status;
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Long getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Long fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Long getApartmentId() {
+        return apartmentId;
+    }
+
+    public void setApartmentId(Long apartmentId) {
+        this.apartmentId = apartmentId;
+    }
+
+    public Long getBuildingId() {
+        return buildingId;
+    }
+
+    public void setBuildingId(Long buildingId) {
+        this.buildingId = buildingId;
+    }
+
+    public Long getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Long cityId) {
+        this.cityId = cityId;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Long getStreetId() {
+        return streetId;
+    }
+
+    public void setStreetId(Long streetId) {
+        this.streetId = streetId;
+    }
+}
---
+++ trunk/src/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/entity/Status.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.complitex.osznconnection.file.entity;
+
+/**
+ *
+ * @author Artem
+ */
+public enum Status {
+
+    UNRESOLVED
+}
