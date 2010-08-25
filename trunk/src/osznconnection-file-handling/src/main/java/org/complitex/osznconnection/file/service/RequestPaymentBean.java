3e3ade2b647614ffb824d13f635ecbbf9560fd51
artkof.work <artkof.work@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2010-08-25 05:08:19 +0000

    request payment related work

--- trunk/src/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/RequestPaymentBean.java
+++ trunk/src/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/RequestPaymentBean.java
@@ -1,7 +1,6 @@
 package org.complitex.osznconnection.file.service;
 
 import java.util.List;
-import java.util.Locale;
 import javax.ejb.Stateless;
 import org.complitex.dictionaryfw.service.AbstractBean;
 import org.complitex.osznconnection.file.entity.RequestPayment;
@@ -16,11 +15,28 @@ public class RequestPaymentBean extends AbstractBean {
 
     private static final String MAPPING_NAMESPACE = RequestPaymentBean.class.getName();
 
+    public enum OrderBy {
+
+        FIRST_NAME("fNam"), MIDDLE_NAME("mName"), LAST_NAME("surNam"), FILE_NAME("fileName"),
+        CITY("internalCity"), STREET("internalStreet"), BUILDING("internalBuilding"), APARTMENT("internalApartment"),
+        STATUS("status");
+
+        private String orderBy;
+
+        private OrderBy(String orderBy) {
+            this.orderBy = orderBy;
+        }
+
+        public String getOrderBy() {
+            return orderBy;
+        }
+    }
+
     public int count(RequestPaymentExample example) {
         return (Integer) sqlSession.selectOne(MAPPING_NAMESPACE + ".count", example);
     }
 
-    public List<RequestPayment> find(RequestPaymentExample example, Locale locale) {
+    public List<RequestPayment> find(RequestPaymentExample example) {
         List<RequestPayment> requests = sqlSession.selectList(MAPPING_NAMESPACE + ".find", example);
         return requests;
     }
