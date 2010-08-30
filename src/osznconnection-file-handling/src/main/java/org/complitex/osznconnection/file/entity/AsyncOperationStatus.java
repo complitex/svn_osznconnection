732a4d4cf52af07c8be640e2e9a1da1148ffd14c
artkof.work <artkof.work@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2010-08-30 10:44:28 +0000

    async operations work progress

---
+++ src/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/entity/AsyncOperationStatus.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.complitex.osznconnection.file.entity;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Artem
+ */
+public class AsyncOperationStatus implements Serializable {
+
+    private int processed;
+
+    private int failed;
+
+    private RequestFile requestFile;
+
+    public AsyncOperationStatus() {
+    }
+
+    public int getFailed() {
+        return failed;
+    }
+
+    public void setFailed(int failed) {
+        this.failed = failed;
+    }
+
+    public int getProcessed() {
+        return processed;
+    }
+
+    public void setProcessed(int processed) {
+        this.processed = processed;
+    }
+
+    public RequestFile getRequestFile() {
+        return requestFile;
+    }
+
+    public void setRequestFile(RequestFile requestFile) {
+        this.requestFile = requestFile;
+    }
+}
