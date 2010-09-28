b40b7bc6d3e41f66d8d4442523cd5afe5736f61f
p.dzubin <p.dzubin@gmail.com>
2010-09-28 06:20:59 +0000

    Fix OSZN codes in ownership correction

--- src/osznconnection-db/import/owner_correction_for_OSZN.sql
+++ src/osznconnection-db/import/owner_correction_for_OSZN.sql
@@ -3,8 +3,8 @@ set @organization_id = 1;
 -- Ownership corrections
 insert into ownership_correction(organization_id, correction, object_id, organization_code) values
 (@organization_id,UPPER('мiсцевих Рад'),1,'1'),
-(@organization_id,UPPER('кооперативна'),2,'1'),
+(@organization_id,UPPER('кооперативна'),2,'4'),
 (@organization_id,UPPER('приватна'),5,'5'),
 (@organization_id,UPPER('приватизована'),6,'6'),
-(@organization_id,UPPER('громадська'),4,'4'),
-(@organization_id,UPPER('вiдомча'),3,'3');
+(@organization_id,UPPER('громадська'),4,'3'),
+(@organization_id,UPPER('вiдомча'),3,'2');
