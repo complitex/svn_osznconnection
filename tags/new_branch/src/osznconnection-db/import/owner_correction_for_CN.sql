9ac9e8b85f5557d2a4abebe34e19de575d13c3ce
heaven.iaa@gmail.com <heaven.iaa@gmail.com@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2010-10-04 05:41:53 +0000

    idea subversion bug

--- tags/new_branch/src/osznconnection-db/import/owner_correction_for_CN.sql
+++
@@ -1,8 +0,0 @@
-set @organization_id = 2;
-
--- Ownership corrections
-insert into ownership_correction(organization_id, correction, object_id, organization_code) values
-(@organization_id,UPPER('ГОС'),1,'6513'),
-(@organization_id,UPPER('КООП'),2,'45188713'),
-(@organization_id,UPPER('ВЫК'),5,'3769903'),
-(@organization_id,UPPER('ЧАС'),6,'6514');
--- tags/new_branch/src/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/exception/AlreadyLoadedException.java
+++
@@ -1,8 +0,0 @@
-package org.complitex.osznconnection.file.service.exception;
-
-/**
- * @author Anatoly A. Ivanov java@inheaven.ru
- *         Date: 02.09.2010 16:53:30
- */
-public class AlreadyLoadedException extends Exception{
-}
