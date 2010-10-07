53b8b055c50ef1609d3900c221e4b3590319433c
heaven.iaa@gmail.com <heaven.iaa@gmail.com@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2010-10-07 07:33:20 +0000

    change to full trunk

--- branches/mdb_process/src/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/exception/MaxErrorCountException.java
+++
@@ -1,9 +0,0 @@
-package org.complitex.osznconnection.file.service.exception;
-
-/**
- * @author Anatoly A. Ivanov java@inheaven.ru
- *         Date: 09.09.2010 15:35:32
- */
-public class MaxErrorCountException extends Exception{
-    
-}
