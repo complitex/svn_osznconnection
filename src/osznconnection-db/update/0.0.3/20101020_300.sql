3d8675ed5e46f0cc71ccde912fd6e71f477bfd24
heaven.iaa <heaven.iaa@gmail.com>
2010-10-22 07:53:41 +0000

    change parent to corrections table

--- src/osznconnection-db/update/0.0.3/20101020_300.sql
+++ src/osznconnection-db/update/0.0.3/20101020_300.sql
@@ -11,15 +11,15 @@ ALTER TABLE `city_correction` ADD COLUMN `parent_id` BIGINT(20) AFTER `object_id
 
 ALTER TABLE `district_correction` ADD COLUMN `parent_id` BIGINT(20) AFTER `object_id`;
 ALTER TABLE `district_correction` ADD KEY `key_parent_id` (`parent_id`);
-ALTER TABLE `district_correction` ADD CONSTRAINT `fk_district_correction__city` FOREIGN KEY (`parent_id`) REFERENCES `city` (`object_id`);
+ALTER TABLE `district_correction` ADD CONSTRAINT `fk_district_correction__city_correction` FOREIGN KEY (`parent_id`) REFERENCES `city_correction` (`id`);
 
 ALTER TABLE `street_correction` ADD COLUMN `parent_id` BIGINT(20) AFTER `object_id`;
 ALTER TABLE `street_correction` ADD KEY `key_parent_id` (`parent_id`);
-ALTER TABLE `street_correction` ADD CONSTRAINT `fk_street_correction__district` FOREIGN KEY (`parent_id`) REFERENCES `district` (`object_id`);
+ALTER TABLE `street_correction` ADD CONSTRAINT `fk_street_correction__district_correction` FOREIGN KEY (`parent_id`) REFERENCES `district_correction` (`id`);
 
 ALTER TABLE `building_correction` ADD COLUMN `parent_id` BIGINT(20) AFTER `object_id`;
 ALTER TABLE `building_correction` ADD KEY `key_parent_id` (`parent_id`);
-ALTER TABLE `building_correction` ADD CONSTRAINT `fk_building_correction__street` FOREIGN KEY (`parent_id`) REFERENCES `street` (`object_id`);
+ALTER TABLE `building_correction` ADD CONSTRAINT `fk_building_correction__street_correction` FOREIGN KEY (`parent_id`) REFERENCES `street_correction` (`id`);
 
 ALTER TABLE `ownership_correction` ADD COLUMN `parent_id` BIGINT(20) AFTER `object_id`;
 ALTER TABLE `privilege_correction` ADD COLUMN `parent_id` BIGINT(20) AFTER `object_id`;
