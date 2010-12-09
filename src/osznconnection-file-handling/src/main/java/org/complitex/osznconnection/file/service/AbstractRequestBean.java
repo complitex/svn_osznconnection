65cf7eecb154448b8d11c7e8eda2e83727394b24
artkof.work <artkof.work@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2010-12-09 10:18:26 +0000

    loading of warnings was enhanced.

--- src/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/AbstractRequestBean.java
+++ src/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/AbstractRequestBean.java
@@ -4,10 +4,8 @@
  */
 package org.complitex.osznconnection.file.service;
 
-import java.util.List;
 import javax.ejb.EJB;
 import org.complitex.dictionaryfw.service.AbstractBean;
-import org.complitex.osznconnection.file.entity.AbstractRequest;
 import org.complitex.osznconnection.file.entity.RequestFile;
 import org.complitex.osznconnection.file.service.warning.RequestWarningBean;
 
@@ -19,16 +17,6 @@ public abstract class AbstractRequestBean extends AbstractBean {
 
     @EJB
     private RequestWarningBean requestWarningBean;
-
-    protected void loadWarnings(AbstractRequest request, RequestFile.TYPE requestFileType) {
-        request.setWarnings(requestWarningBean.getWarnings(request.getId(), requestFileType));
-    }
-
-    protected void loadWarnings(List<? extends AbstractRequest> requests, RequestFile.TYPE requestFileType) {
-        for (AbstractRequest request : requests) {
-            loadWarnings(request, requestFileType);
-        }
-    }
 
     protected void clearWarnings(long requestFileId, RequestFile.TYPE requestFileType) {
         requestWarningBean.delete(requestFileId, requestFileType);
