5cad721e34612ce0c1f9fcf4fa131d5a97a1819c
heaven.iaa <heaven.iaa@gmail.com>
2010-12-10 12:30:37 +0000

    replace load page by load dialog panel, move request file process settings to admin menu

--- src/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/web/RequestFileLoadPanel.java
+++ src/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/web/RequestFileLoadPanel.java
@@ -1,11 +1,10 @@
 package org.complitex.osznconnection.file.web;
 
-import org.apache.wicket.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
@@ -14,40 +13,50 @@ import org.complitex.dictionaryfw.web.component.DisableAwareDropDownChoice;
 import org.complitex.dictionaryfw.web.component.DomainObjectDisableAwareRenderer;
 import org.complitex.dictionaryfw.web.component.MonthDropDownChoice;
 import org.complitex.dictionaryfw.web.component.YearDropDownChoice;
-import org.complitex.osznconnection.commons.web.pages.welcome.WelcomePage;
-import org.complitex.osznconnection.commons.web.security.SecurityRole;
-import org.complitex.osznconnection.commons.web.template.FormTemplatePage;
 import org.complitex.osznconnection.file.service.process.ProcessManagerBean;
 import org.complitex.osznconnection.organization.strategy.OrganizationStrategy;
+import org.odlabs.wiquery.core.javascript.JsStatement;
+import org.odlabs.wiquery.ui.core.JsScopeUiEvent;
+import org.odlabs.wiquery.ui.dialog.Dialog;
 
 import javax.ejb.EJB;
 import java.util.List;
 
 /**
  * @author Anatoly A. Ivanov java@inheaven.ru
- *         Date: 25.08.2010 15:43:27
- *
- * @deprecated replaced by RequestFileLoadPanel
+ *         Date: 10.12.10 16:52
  */
-@AuthorizeInstantiation(SecurityRole.AUTHORIZED)
-@Deprecated
-public class RequestFileLoad extends FormTemplatePage {
-
+public class RequestFileLoadPanel extends Panel {
     @EJB(name = "ProcessManagerBean")
     private ProcessManagerBean processManagerBean;
 
     @EJB(name = "OrganizationStrategy")
     private OrganizationStrategy organizationStrategy;
 
-    public RequestFileLoad() {
-        super();
+    private final Dialog dialog;
 
-        add(new Label("title", getString("title")));
-        add(new FeedbackPanel("messages"));
+    public static interface ILoader{
+        void load(Long organizationId, String districtCode, int monthFrom, int monthTo, int year);
+    }
+
+    public RequestFileLoadPanel(String id, final ILoader loader) {
+        super(id);
+
+        dialog = new Dialog("dialog");
+        dialog.setModal(true);
+        dialog.setWidth(735);
+        dialog.setOpenEvent(JsScopeUiEvent.quickScope(new JsStatement().self().chain("parents", "'.ui-dialog:first'").
+                chain("find", "'.ui-dialog-titlebar-close'").
+                chain("hide").render()));
+        dialog.setOutputMarkupId(true);
+        dialog.setOutputMarkupPlaceholderTag(true);
+        add(dialog);
+
+        dialog.add(new FeedbackPanel("messages"));
 
         //Форма
         Form form = new Form("form");
-        add(form);
+        dialog.add(form);
 
         //Организация
         IModel<List<DomainObject>> osznsModel = new LoadableDetachableModel<List<DomainObject>>() {
@@ -99,66 +108,33 @@ public class RequestFileLoad extends FormTemplatePage {
 
                 if (!processManagerBean.isProcessing()) {
                     DomainObject oszn = organizationModel.getObject();
-                    processManagerBean.loadGroup(oszn.getId(), organizationStrategy.getDistrictCode(oszn), f, t,
-                            year.getModelObject());
+                    loader.load(oszn.getId(), organizationStrategy.getDistrictCode(oszn), f, t, year.getModelObject());
                     getSession().info(getString("info.start_loading"));
                 } else {
                     getSession().error(getString("error.loading_in_progress"));
                 }
 
-                setResponsePage(GroupList.class);
-
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    //eh...
-                }
+                dialog.setAutoOpen(false);
+                dialog.close();
             }
         };
         form.add(load);
-
-        //Загрузить
-        Button loadTarif = new Button("load_tarif") {
-
-            @Override
-            public void onSubmit() {
-                int f = from.getModelObject();
-                int t = to.getModelObject();
-
-                if (t < f) {
-                    error(getString("error.to_less_then_from"));
-                    return;
-                }
-
-                if (!processManagerBean.isProcessing()) {
-                    DomainObject oszn = organizationModel.getObject();
-                    processManagerBean.loadTarif(oszn.getId(), organizationStrategy.getDistrictCode(oszn), f, t,
-                            year.getModelObject());
-                    getSession().info(getString("info.tarif_start_loading"));
-                } else {
-                    getSession().error(getString("error.loading_in_progress"));
-                }
-
-                setResponsePage(TarifFileList.class);
-
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    //eh...
-                }
-            }
-        };
-        form.add(loadTarif);
 
         //Отмена
         Button cancel = new Button("cancel") {
 
             @Override
             public void onSubmit() {
-                setResponsePage(WelcomePage.class);
+                dialog.setAutoOpen(false);
+                dialog.close();
             }
         };
         cancel.setDefaultFormProcessing(false);
         form.add(cancel);
     }
+
+    public void open(){
+        dialog.setAutoOpen(true);
+    }
 }
+
