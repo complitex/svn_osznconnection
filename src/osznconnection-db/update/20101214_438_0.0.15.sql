03aed0637e7a3902209bbd32aaef1bc968900c96
artkof.work <artkof.work@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2010-12-14 07:41:12 +0000

    attempt to remedy deadlock problem.

--- src/osznconnection-db/update/20101214_438_0.0.15.sql
+++ src/osznconnection-db/update/20101214_438_0.0.15.sql
@@ -1,6 +1,6 @@
 -- Script adds request_warning index.
 
-ALTER TABLE `request_warning` ADD UNIQUE KEY `uk_request_warning` (`request_id`, `request_file_type`);
+ALTER TABLE `request_warning` ADD KEY `key_request_warning__request` (`request_id`), ADD KEY `key_request_warning__request_file` (`request_file_type`);
 
 INSERT INTO `update` (`version`) VALUE ('20101214_438');
 
