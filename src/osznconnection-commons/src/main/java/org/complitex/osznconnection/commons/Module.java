d64257104bb682c9bfff223f82b7acea407ee2df
heaven.iaa <heaven.iaa@gmail.com>
2010-12-15 09:32:15 +0000

    moved to http://complitex.googlecode.com/svn/

--- src/osznconnection-commons/src/main/java/org/complitex/osznconnection/commons/Module.java
+++
@@ -1,9 +0,0 @@
-package org.complitex.osznconnection.commons;
-
-/**
- * @author Anatoly A. Ivanov java@inheaven.ru
- *         Date: 17.08.2010 18:41:01
- */
-public class Module {
-    public static final String NAME = "org.complitex.osznconnection.commons";
-}
