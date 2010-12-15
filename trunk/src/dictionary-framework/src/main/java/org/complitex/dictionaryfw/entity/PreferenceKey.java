15f1bc710b97cdf0644c15535880d0905d0f1553
heaven.iaa@gmail.com <heaven.iaa@gmail.com@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2010-12-15 09:32:15 +0000

    moved to http://complitex.googlecode.com/svn/

--- trunk/src/dictionary-framework/src/main/java/org/complitex/dictionaryfw/entity/PreferenceKey.java
+++
@@ -1,9 +0,0 @@
-package org.complitex.dictionaryfw.entity;
-
-/**
- * @author Anatoly A. Ivanov java@inheaven.ru
- *         Date: 30.11.10 17:14
- */
-public enum PreferenceKey {
-    SORT_PROPERTY, SORT_ORDER, FILTER_OBJECT, PAGE_INDEX, ROWS_PER_PAGE, LOCALE
-}
