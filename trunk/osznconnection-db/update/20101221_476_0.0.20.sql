220258d5d7154e18fad0729ce8aa7a44b3ef5f8f
artkof.work <artkof.work@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2010-12-21 09:07:18 +0000

    some i18n messages were corrected.

--- trunk/osznconnection-db/update/20101221_476_0.0.20.sql
+++ trunk/osznconnection-db/update/20101221_476_0.0.20.sql
@@ -6,5 +6,6 @@ UPDATE `status_description` SET `name` = 'Нечисловой код формы
 UPDATE `status_description` SET `name` = 'Объект льготы не найден в справочнике соответствий для МН' WHERE `code` = 304;
 UPDATE `status_description` SET `name` = 'Код льготы не найден в справочнике соответствий для ОСЗН' WHERE `code` = 305;
 UPDATE `status_description` SET `name` = 'Нечисловой код льготы в справочнике соответствий для ОСЗН' WHERE `code` = 306;
+UPDATE `status_description` SET `name` = 'Льгота не найдена в справочнике соответствий' WHERE `code` = 218;
 
 INSERT INTO `update` (`version`) VALUE ('20101221_476');
