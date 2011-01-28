97d6be2d503abad67e8bed8fe4222a8c841125d2
heaven.iaa <heaven.iaa@gmail.com>
2011-01-28 07:40:50 +0000

    add unique key for permission, rename methods

--- osznconnection-db/create.sql
+++ osznconnection-db/create.sql
@@ -1849,7 +1849,7 @@ CREATE TABLE `permission` (
     `entity` VARCHAR(64) NOT NULL,
     `object_id` BIGINT(20) NOT NULL,
     PRIMARY KEY (`pk_id`),
-    UNIQUE KEY `key_unique` (`permission_id`, `table`, `entity`, `object_id`),
+    UNIQUE KEY `key_unique` (`permission_id`, `entity`, `object_id`),
     KEY `key_permission_id` (`permission_id`),
     KEY `key_table` (`table`),
     KEY `key_entity` (`entity`),
