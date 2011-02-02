8342b47709d0d3bcf1dd3e7e7a422366f001d7d4
artkof.work <artkof.work@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2011-02-02 09:32:42 +0000

    work on permissions for address entities in progress. Propagation of visibility rights down in object tree has been added.

--- trunk/osznconnection-db/update/20110126_520_0.1.1.sql
+++ trunk/osznconnection-db/update/20110126_520_0.1.1.sql
@@ -8,7 +8,7 @@ CREATE TABLE `permission` (
     `table` VARCHAR(64) NOT NULL,
     `entity` VARCHAR(64) NOT NULL,
     `object_id` BIGINT(20) NOT NULL,
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`pk_id`),
     UNIQUE KEY `key_unique` (`permission_id`, `entity`, `object_id`),
     KEY `key_permission_id` (`permission_id`),
     KEY `key_table` (`table`),
@@ -17,6 +17,7 @@ CREATE TABLE `permission` (
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 INSERT INTO `permission` (`permission_id`, `table`, `entity`, `object_id`) VALUE (0, 'ALL', 'ALL', 0);
+INSERT INTO `sequence` (`sequence_name`, `sequence_value`) VALUES ('permission', 1);
 
 CREATE TABLE `user_organization` (
     `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
@@ -28,7 +29,13 @@ CREATE TABLE `user_organization` (
     CONSTRAINT `fk_user_organization__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
     CONSTRAINT `fk_user_organization__organization` FOREIGN KEY (`organization_object_id`)
       REFERENCES `organization` (`object_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+INSERT INTO `string_culture`(`id`, `locale_id`, `value`) VALUES (907, 1, UPPER('Принадлежит')), (907, 2, UPPER('Принадлежит'));
+INSERT INTO `entity_attribute_type`(`id`, `entity_id`, `mandatory`, `attribute_type_name_id`, `system`) VALUES (903, 900, 0, 907, 1);
+INSERT INTO `entity_attribute_value_type`(`id`, `attribute_type_id`, `attribute_value_type`) VALUES (903, 903, 'organization');
+INSERT INTO `string_culture`(`id`, `locale_id`, `value`) VALUES (906, 1, UPPER('Организации пользователей')), (906, 2, UPPER('Организации пользователей'));
+INSERT INTO `entity_type`(`id`, `entity_id`, `entity_type_name_id`) VALUES (902, 900, 906);
   
 ALTER TABLE `apartment` ADD COLUMN `permission_id` BIGINT(20) NOT NULL DEFAULT 0,
 ADD KEY `key_permission_id` (`permission_id`), 
@@ -85,17 +92,5 @@ ADD CONSTRAINT `fk_ownership__permission` FOREIGN KEY (`permission_id`) REFERENC
 ALTER TABLE `privilege` ADD COLUMN `permission_id` BIGINT(20) NOT NULL DEFAULT 0,
 ADD KEY `key_permission_id` (`permission_id`), 
 ADD CONSTRAINT `fk_privilege__permission` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`);
-
-ALTER TABLE `user_info` ADD COLUMN `permission_id` BIGINT(20) NOT NULL DEFAULT 0,
-ADD KEY `key_permission_id` (`permission_id`),
-ADD CONSTRAINT `fk_user_info__permission` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`);
-
-ALTER TABLE `request_file` ADD COLUMN `permission_id` BIGINT(20) NOT NULL DEFAULT 0,
-ADD KEY `key_permission_id` (`permission_id`),
-ADD CONSTRAINT `fk_request_file__permission` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`);
-
-ALTER TABLE `request_file_group` ADD COLUMN `permission_id` BIGINT(20) NOT NULL DEFAULT 0,
-ADD KEY `key_permission_id` (`permission_id`),
-ADD CONSTRAINT `fk_request_file_group__permission` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`);
 
 INSERT INTO `update` (`version`) VALUE ('20110126_520_0.1.1');
