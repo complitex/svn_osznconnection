bedd9fe331b7adc3cf9e874b8a598410b4795b0e
heaven.iaa <heaven.iaa@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2011-02-02 10:44:14 +0000

    add employees child view group

---
+++ trunk/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/entity/SecurityRole.java
@@ -0,0 +1,10 @@
+package org.complitex.osznconnection.file.entity;
+
+/**
+ * @author Anatoly A. Ivanov java@inheaven.ru
+ *         Date: 02.02.11 15:06
+ */
+public enum SecurityRole {
+    AUTHORIZED,
+    ADMIN_MODULE_VIEW
+}
