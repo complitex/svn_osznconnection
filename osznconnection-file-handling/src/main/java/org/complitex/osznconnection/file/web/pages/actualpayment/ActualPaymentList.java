702ecdf29fe2fe0dba66105c03036759a8b8f01d
heaven.iaa <heaven.iaa@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2011-02-11 10:17:23 +0000

    add order by directory to actual payment list and tarif list, fix admin to page edit access check

--- osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/web/pages/actualpayment/ActualPaymentList.java
+++ osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/web/pages/actualpayment/ActualPaymentList.java
@@ -110,7 +110,7 @@ public final class ActualPaymentList extends TemplatePage {
         final RequestFile requestFile = requestFileBean.findById(fileId);
 
         //Проверка доступа к данным
-        if (!osznSessionBean.hasOuterOrganization(requestFile.getOrganizationId())) {
+        if (!osznSessionBean.isAuthorized(requestFile.getOrganizationId())) {
             throw new UnauthorizedInstantiationException(this.getClass());
         }
 
