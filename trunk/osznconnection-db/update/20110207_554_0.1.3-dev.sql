ea2cb927e4369ef950eb9bd8b5a585b6729fa91d
heaven.iaa <heaven.iaa@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2011-02-14 13:05:36 +0000

    add tables for name dictionary

--- trunk/osznconnection-db/update/20110207_554_0.1.3-dev.sql
+++ trunk/osznconnection-db/update/20110207_554_0.1.3-dev.sql
@@ -9,3 +9,36 @@ DROP FOREIGN KEY `fk_request_file_group__permission`;
 ALTER TABLE `request_file` DROP COLUMN `permission_id`,
 DROP KEY `key_permission_id`,
 DROP FOREIGN KEY `fk_request_file__permission`;
+
+-- ------------------------------
+-- First Name
+-- ------------------------------
+CREATE TABLE `first_name` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `key_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ------------------------------
+-- Middle Name
+-- ------------------------------
+CREATE TABLE `middle_name` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `key_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ------------------------------
+-- Last Name
+-- ------------------------------
+CREATE TABLE `last_name` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `key_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- uncomment and update on release
+-- INSERT INTO `update` (`version`) VALUES ('20110207_575_0.1.3');
