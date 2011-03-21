d50a7c798cdbef9d3f6166003927883497cd26d1
artkof.work <artkof.work@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2011-03-21 07:03:54 +0000

    Deletion of entity objects has been added.

--- osznconnection-db/update/20110321_629_0.1.7.sql
+++ osznconnection-db/update/20110321_629_0.1.7.sql
@@ -1,4 +1,4 @@
--- Adds support for actual payments.
+-- Adds foreign keys to person_account table.
 ALTER TABLE `person_account` ADD KEY `key_oszn_id` (`oszn_id`), ADD KEY `key_calc_center_id` (`calc_center_id`),
         ADD CONSTRAINT `fk_person_account__osnz_organization` FOREIGN KEY (`oszn_id`) REFERENCES `organization` (`object_id`),
         ADD CONSTRAINT `fk_person_account__calc_center_organization` FOREIGN KEY (`calc_center_id`) REFERENCES `organization` (`object_id`);
