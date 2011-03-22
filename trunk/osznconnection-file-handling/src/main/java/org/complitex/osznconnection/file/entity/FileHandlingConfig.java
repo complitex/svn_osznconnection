8895744cc9876bee588aeb7e9710fb0e7dd17d03
heaven.iaa <heaven.iaa@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2011-03-22 12:08:28 +0000

    add config group

--- trunk/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/entity/FileHandlingConfig.java
+++ trunk/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/entity/FileHandlingConfig.java
@@ -7,41 +7,48 @@ import org.complitex.dictionary.entity.IConfig;
  *         Date: 04.10.2010 11:12:46
  */
 public enum FileHandlingConfig implements IConfig{
-    LOAD_INPUT_REQUEST_FILE_STORAGE_DIR("c:\\storage\\subsidy_in"),
-    SAVE_OUTPUT_REQUEST_FILE_STORAGE_DIR("c:\\storage\\subsidy_out"),
+    LOAD_INPUT_REQUEST_FILE_STORAGE_DIR("c:\\storage\\subsidy_in", "storage"),
+    SAVE_OUTPUT_REQUEST_FILE_STORAGE_DIR("c:\\storage\\subsidy_out", "storage"),
 
-    LOAD_INPUT_ACTUAL_PAYMENT_FILE_STORAGE_DIR("c:\\storage\\actual_in"),
-    SAVE_OUTPUT_ACTUAL_PAYMENT_FILE_STORAGE_DIR("c:\\storage\\actual_out"),
+    LOAD_INPUT_ACTUAL_PAYMENT_FILE_STORAGE_DIR("c:\\storage\\actual_in", "storage"),
+    SAVE_OUTPUT_ACTUAL_PAYMENT_FILE_STORAGE_DIR("c:\\storage\\actual_out", "storage"),
 
-    PAYMENT_FILENAME_PREFIX("A_"),
-    BENEFIT_FILENAME_PREFIX("AF"),
-    PAYMENT_BENEFIT_FILENAME_SUFFIX("\\d{4}{MM}\\.DBF"),
-    ACTUAL_PAYMENT_FILENAME_MASK(".*{MM}{YY}\\.DFB"),
-    TARIF_PAYMENT_FILENAME_MASK("TARIF12\\.DBF"),
+    PAYMENT_FILENAME_PREFIX("A_", "mask"),
+    BENEFIT_FILENAME_PREFIX("AF", "mask"),
+    PAYMENT_BENEFIT_FILENAME_SUFFIX("\\d{4}{MM}\\.DBF", "mask"),
+    ACTUAL_PAYMENT_FILENAME_MASK(".*{MM}{YY}\\.DFB", "mask"),
+    TARIF_PAYMENT_FILENAME_MASK("TARIF12\\.DBF", "mask"),
 
-    LOAD_THREAD_SIZE("2"),
-    BIND_THREAD_SIZE("4"),
-    FILL_THREAD_SIZE("4"),
-    SAVE_THREAD_SIZE("4"),
+    LOAD_THREAD_SIZE("2", "thread"),
+    BIND_THREAD_SIZE("4", "thread"),
+    FILL_THREAD_SIZE("4", "thread"),
+    SAVE_THREAD_SIZE("4", "thread"),
 
-    LOAD_BATCH_SIZE("16"),
-    BIND_BATCH_SIZE("64"),
-    FILL_BATCH_SIZE("64"),
+    LOAD_BATCH_SIZE("16", "batch"),
+    BIND_BATCH_SIZE("64", "batch"),
+    FILL_BATCH_SIZE("64", "batch"),
 
-    LOAD_MAX_ERROR_COUNT("10"),
-    BIND_MAX_ERROR_COUNT("10"),
-    FILL_MAX_ERROR_COUNT("10"),
-    SAVE_MAX_ERROR_COUNT("10");
+    LOAD_MAX_ERROR_COUNT("10", "error"),
+    BIND_MAX_ERROR_COUNT("10", "error"),
+    FILL_MAX_ERROR_COUNT("10", "error"),
+    SAVE_MAX_ERROR_COUNT("10", "error");
 
     private String defaultValue;
+    private String groupKey;
 
-    FileHandlingConfig(String defaultValue) {
+    FileHandlingConfig(String defaultValue, String groupKey) {
         this.defaultValue = defaultValue;
+        this.groupKey = groupKey;
     }
 
     @Override
     public String getDefaultValue() {
         return defaultValue;
+    }
+
+    @Override
+    public String getGroupKey() {
+        return groupKey;
     }
 }
 
