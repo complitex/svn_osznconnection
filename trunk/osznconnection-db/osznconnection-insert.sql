aa65c0bbd83440d74ccc3cc5b965538217f15a40
heaven.iaa <heaven.iaa@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2011-04-08 09:36:42 +0000

    add current database version

--- trunk/osznconnection-db/osznconnection-insert.sql
+++ trunk/osznconnection-db/osznconnection-insert.sql
@@ -297,3 +297,6 @@ INSERT INTO `organization_string_culture`(`id`, `locale_id`, `value`) VALUES
 (1, 1, UPPER('Модуль №1')), (1,2,UPPER('Модуль №1')), (2, 1, UPPER('0'));
 INSERT INTO `organization_attribute`(`attribute_id`, `object_id`, `attribute_type_id`, `value_id`, `value_type_id`) VALUES
 (1,0,900,1,900), (1,0,901,2,901);
+
+-- Current database version
+INSERT INTO `update` (`version`) VALUE ('20110329_646_0.1.9');
