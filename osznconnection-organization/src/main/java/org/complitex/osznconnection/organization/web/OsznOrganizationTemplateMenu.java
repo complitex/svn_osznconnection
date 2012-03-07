1e1b74754672b35865dfbe080a992c42440ec33b
artkof.work <artkof.work@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2012-03-07 06:35:05 +0000

    TemplateLoader has been improved so that now it makes possible to specify home page class in template-config.xml. MenuManager has been added. It enable server side to change selected menu item and hide/open main menu in UI. TemplatePage and ProfilePage have been refined in order to support custom application navigation to and from profile page. OrganizationTemplateMenu was renamed to OrganizationMenu.

--- osznconnection-organization/src/main/java/org/complitex/osznconnection/organization/web/OsznOrganizationTemplateMenu.java
+++ osznconnection-organization/src/main/java/org/complitex/osznconnection/organization/web/OsznOrganizationTemplateMenu.java
@@ -3,7 +3,7 @@ package org.complitex.osznconnection.organization.web;
 import org.apache.wicket.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.complitex.dictionary.strategy.IStrategy;
 import org.complitex.dictionary.util.EjbBeanLocator;
-import org.complitex.organization.web.OrganizationTemplateMenu;
+import org.complitex.organization.web.OrganizationMenu;
 import org.complitex.osznconnection.organization.strategy.OsznOrganizationStrategy;
 import org.complitex.template.web.security.SecurityRole;
 
@@ -12,7 +12,7 @@ import org.complitex.template.web.security.SecurityRole;
  * @author Artem
  */
 @AuthorizeInstantiation(SecurityRole.AUTHORIZED)
-public class OsznOrganizationTemplateMenu extends OrganizationTemplateMenu {
+public class OsznOrganizationTemplateMenu extends OrganizationMenu {
 
     @Override
     protected IStrategy getStrategy() {
