5f94b65587487ff0dbb0ac8c1e25f18901acc9b6
artkof.work <artkof.work@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2012-04-05 10:34:10 +0000

    Oszn organization module detection has been fixed.

---
+++ osznconnection-organization/src/main/java/org/complitex/osznconnection/organization/OsznOrganizationModule.java
@@ -0,0 +1,45 @@
+package org.complitex.osznconnection.organization;
+
+import java.util.Map.Entry;
+import org.apache.wicket.markup.html.WebPage;
+
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import org.apache.wicket.PageParameters;
+import org.complitex.dictionary.strategy.organization.IOrganizationStrategy;
+import org.complitex.organization.DefaultOrganizationModule;
+import org.complitex.organization.IOrganizationModule;
+
+@Singleton(name = DefaultOrganizationModule.CUSTOM_ORGANIZATION_MODULE_BEAN_NAME)
+@Startup
+public class OsznOrganizationModule implements IOrganizationModule {
+
+    @EJB(name = "OsznOrganizationStrategy")
+    private IOrganizationStrategy osznOrganizationStrategy;
+    public static final String NAME = "org.complitex.osznconnection.organization";
+
+    @Override
+    public Class<? extends WebPage> getEditPage() {
+        return osznOrganizationStrategy.getEditPage();
+    }
+
+    @Override
+    public String getEditPageParams() {
+        final StringBuilder builder = new StringBuilder();
+        PageParameters pageParameters = osznOrganizationStrategy.getEditPageParams(null, null, null);
+        for (Entry<String, Object> entry : pageParameters.entrySet()) {
+            final String key = entry.getKey();
+            final Object value = entry.getValue();
+            if (value instanceof String) {
+                builder.append(key).append("=").append(value).append(",");
+            }
+        }
+
+        //remove last symbol '&'
+        if (builder.length() > 0) {
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        return builder.toString();
+    }
+}
