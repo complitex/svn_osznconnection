9d6aad1fe415b016945136f0d1bd4708002c707d
heaven.iaa <heaven.iaa@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2013-11-13 07:04:02 +0000

    fix create pool script

--- trunk/osznconnection-db/create-pool.sh
+++ trunk/osznconnection-db/create-pool.sh
@@ -1,19 +1,21 @@
-GLASSFISH_ASADMIN=C:\glassfishv3\glassfish\bin\asadmin
+#!/bin/sh
+
+GLASSFISH_ASADMIN=asadmin
 
 echo ---------------------------------------------------
 echo Local database and Realm
 echo ---------------------------------------------------
 echo
 echo Register the JDBC connection pool
-exec $GLASSFISH_ASADMIN create-jdbc-connection-pool --driverclassname com.mysql.jdbc.Driver --restype java.sql.Driver --property url=jdbc\:mysql\://localhost\:3306/osznconnection:user=osznconnection:password=osznconnection osznconnectionPool
+$GLASSFISH_ASADMIN create-jdbc-connection-pool --datasourceclassname="com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource" --restype="javax.sql.ConnectionPoolDataSource" --property="url=jdbc\:mysql\://localhost\:3306/osznconnection:user=osznconnection:password=osznconnection:characterResultSets=utf8:characterEncoding=utf8:useUnicode=true" osznconnectionPool
 
 echo
 echo Create a JDBC resource with the specified JNDI name
-exec $GLASSFISH_ASADMIN create-jdbc-resource --connectionpoolid osznconnectionPool jdbc/osznconnectionResource
+$GLASSFISH_ASADMIN create-jdbc-resource --connectionpoolid osznconnectionPool jdbc/osznconnectionResource
 
 echo
 echo Add the named authentication realm
-exec $GLASSFISH_ASADMIN create-auth-realm --classname com.sun.enterprise.security.auth.realm.jdbc.JDBCRealm --property jaas-context=jdbcRealm:datasource-jndi=jdbc/osznconnectionResource:user-table=user:user-name-column=login:password-column=password:group-table=usergroup:group-name-column=group_name:charset=UTF-8 osznconnectionRealm
+$GLASSFISH_ASADMIN create-auth-realm --classname="com.sun.enterprise.security.auth.realm.jdbc.JDBCRealm" --property="jaas-context=jdbcRealm:datasource-jndi=jdbc/osznconnectionResource:user-table=user:user-name-column=login:password-column=password:group-table=usergroup:group-name-column=group_name:charset=UTF-8:digest-algorithm=MD5" osznconnectionRealm
  
 echo
 echo ---------------------------------------------------
@@ -21,8 +23,8 @@ echo Remote database
 echo ---------------------------------------------------
 echo
 echo Register the JDBC connection pool
-exec $GLASSFISH_ASADMIN create-jdbc-connection-pool --driverclassname oracle.jdbc.OracleDriver --restype java.sql.Driver --property url=jdbc\:oracle\:thin\:@192.168.1.102\:1521\:cnhar:user=comp:password=comp osznconnectionRemotePool
+$GLASSFISH_ASADMIN create-jdbc-connection-pool --driverclassname oracle.jdbc.OracleDriver --restype java.sql.Driver --property url=jdbc\:oracle\:thin\:@192.168.1.102\:1521\:cnhar:user=comp:password=comp osznconnectionRemotePool
 
 echo
 echo Create a JDBC resource with the specified JNDI name
-exec $GLASSFISH_ASADMIN create-jdbc-resource --connectionpoolid osznconnectionRemotePool jdbc/osznconnectionRemoteResource
+$GLASSFISH_ASADMIN create-jdbc-resource --connectionpoolid osznconnectionRemotePool jdbc/osznconnectionRemoteResource
