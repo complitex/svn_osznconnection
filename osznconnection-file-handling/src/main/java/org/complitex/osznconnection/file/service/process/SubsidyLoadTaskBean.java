c66e30e0eafd63efb51592210ac5f5fafb584115
heaven.iaa <heaven.iaa@gmail.com>
2013-12-05 05:08:54 +0000

    add summa validate by numm

--- osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/process/SubsidyLoadTaskBean.java
+++ osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/process/SubsidyLoadTaskBean.java
@@ -63,10 +63,19 @@ public class SubsidyLoadTaskBean implements ITaskBean {
 
                     BigDecimal nSum = new BigDecimal(0);
                     BigDecimal sbSum = new BigDecimal(0);
+                    BigDecimal smSum = new BigDecimal(0);
+
+                    /*
+                       Если поле NUMM != 0, то нужно выполнять проверку SUMMA == SUBS * NUMM и SUMMA должен быть
+                       вточности равен сумме полей SBN, где N - коды услуг, разрешенных для организации пользователей.
+                       Если поле NUMM == 0, то нужно выполнять проверку SUMMA должен быть вточности равен сумме полей SMN,
+                       где N - коды услуг, разрешенных для организации пользователей.
+                     */
 
                     for (ServiceAssociation sa : organization.getServiceAssociationList()) {
                         nSum = nSum.add((BigDecimal) request.getField("P" + sa.getServiceProviderTypeId()));
                         sbSum = sbSum.add((BigDecimal) request.getField("SB" + sa.getServiceProviderTypeId()));
+                        smSum = smSum.add((BigDecimal) request.getField("SM" + sa.getServiceProviderTypeId()));
                     }
 
                     Long numm = (Long)request.getField("NUMM");
@@ -74,8 +83,9 @@ public class SubsidyLoadTaskBean implements ITaskBean {
                     BigDecimal subs = (BigDecimal) request.getField("SUBS");
 
                     if (!request.getField("NM_PAY").equals(nSum.setScale(2))
-                            || !summa.equals(sbSum.setScale(2))
-                            || (numm != 0 && !summa.equals(subs.multiply(new BigDecimal(numm))))) {
+                            || (numm != 0 && !summa.equals(subs.multiply(new BigDecimal(numm)))
+                                && !summa.equals(sbSum.setScale(2)))
+                            || (numm == 0 && !summa.equals(smSum.setScale(2)))) {
                         request.setStatus(RequestStatus.SUBSIDY_NM_PAY_ERROR);
                         requestFile.setStatus(RequestFileStatus.LOAD_ERROR);
                     }
