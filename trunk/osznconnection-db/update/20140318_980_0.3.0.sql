4189c3013c61bf1e4c342497b85d8aa19bc24677
heaven.iaa <heaven.iaa@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2014-03-20 16:25:32 +0000

    update building import

---
+++ trunk/osznconnection-db/update/20140318_980_0.3.0.sql
@@ -0,0 +1,7 @@
+-- --------------------------------
+-- Current database version
+-- --------------------------------
+INSERT INTO `update` (`version`) VALUE ('20140318_980_0.3.0');
+
+drop table `building_segment_import`;
+drop table `building_import`;
