c18420262317e477392ef2d4eb4aae0820f855f7
heaven.iaa@gmail.com <heaven.iaa@gmail.com@3fe1a85b-187a-ca64-f1fb-a7778d9068d8>
2014-04-08 16:15:49 +0000

    fix TO_CYRILLIC

--- trunk/osznconnection-db/osznconnection-functions.sql
+++ trunk/osznconnection-db/osznconnection-functions.sql
@@ -1,14 +1,10 @@
 DROP FUNCTION IF EXISTS `TO_CYRILLIC`/
 
-CREATE FUNCTION `TO_CYRILLIC`(str VARCHAR(1000)) RETURNS VARCHAR(1000)
+CREATE FUNCTION `TO_CYRILLIC`(str VARCHAR(1000)) RETURNS VARCHAR(1000) CHARSET utf8 COLLATE utf8_unicode_ci
 NO SQL
-  BEGIN
-    IF str IS NULL THEN
-      RETURN NULL;
-    END IF;
+  BEGIN IF str IS NULL THEN RETURN NULL; END IF;
     RETURN REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
-      REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
-      str, 'a', 'а'), 'A', 'А'), 'T', 'Т'), 'x', 'х'), 'X', 'Х'), 'k', 'к'), 'K', 'К'), 'M', 'М'), 'e', 'е'), 'E', 'Е'),
-      'o', 'о'), 'O', 'О'), 'p', 'р'), 'P', 'Р'), 'c', 'с'), 'C', 'С'), 'B', 'В'), 'і', 'i'), 'І', 'I'), 'Ї', 'Є'),
-      'Ў', 'I'), '∙', 'ї'), '°', 'Ї');
+      REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE( str, 'a', 'а'), 'A', 'А'), 'T', 'Т'),
+      'x', 'х'), 'X', 'Х'), 'k', 'к'), 'K', 'К'), 'M', 'М'), 'e', 'е'), 'E', 'Е'), 'o', 'о'), 'O', 'О'), 'p', 'р'), 'P', 'Р'),
+      'c', 'с'), 'C', 'С'), 'B', 'В'), 'i', 'і'), 'I', 'І'), 'Ї', 'Є'), 'Ў', 'І'), '∙', 'ї'), '°', 'Ї');
   END/
